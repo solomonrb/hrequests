e4f9a65b5b9e39ecff885ca227359626dc75833f
dakingsol <solomonrb@alumni.brown.edu>
2025-04-11 16:01:11 -0400

    turn off geoip for override

--- hrequests/browser/firefox.py
+++ hrequests/browser/firefox.py
@@ -11,7 +11,7 @@ class FirefoxBrowserClient(AbstractBrowserClient):
         # Create a Camoufox browser
         browser = await AsyncNewBrowser(
             self.engine.playwright,
-            geoip=launch_args.get('geoip', bool(self.proxy)),
+            geoip=False,
             proxy=self.proxy,
             ff_version=launch_args.pop('version', None),
             i_know_what_im_doing=True,
